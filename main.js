9cfcb2099af84057305e337b03e395b62b9ff0a1
Komal183 <43087848+Komal183@users.noreply.github.com>
2021-01-10 12:34:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function getPara1()
+{
+	var inputs = [];
+	for(var i = 1 ; i <=6 ; i++)
+	{
+		inputs.push(document.getElementById("para1_input_box_" + i).value);
+	}
+document.getElementById("showPara1").innerHTML = inputs.join(". ");
+}
+
+function getPara2()
+{
+	var inputs = [];
+	for(var i = 1 ; i <=6 ; i++)
+	{
+		inputs.push(document.getElementById("para2_input_box_" + i).value);
+	}
+document.getElementById("showPara2").innerHTML = inputs.join(". ");
+}
